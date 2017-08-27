f27ab50e758011d9b00e962a9977f39f6b7b6719
progresivoJS <progresivoJS@gmail.com>
2017-08-27 01:45:52 +0000

    2156 solved

---
+++ DP/2156/Main.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+    private static int[] alchol;
+    private static int n;
+    private static int[][] cache;
+    public static int solve(int n, int[] alchol)
+    {
+        Main.alchol = alchol;
+        Main.n = n;
+        
+        cache = new int[n][3];
+        for (int i = 0; i < cache.length; i++)
+            Arrays.fill(cache[i], -1);
+        
+        int result = Math.max(drink(0, 1), drink(1, 1));
+        return result;
+    }
+    
+    /**
+     * index 번째 술을 마신다고 가정했을 때, 최댓값.
+     */
+    private static int drink(int index, int count)
+    {
+        if (index >= n)
+            return 0;
+            
+        if (cache[index][count] != -1)
+            return cache[index][count];
+        
+        int result = -1;
+        if (count < 2)
+            result = Math.max(result, alchol[index] + drink(index + 1, 2));
+        result = Math.max(result, alchol[index] + drink(index + 2, 1));
+        result = Math.max(result, alchol[index] + drink(index + 3, 1));
+        
+        return cache[index][count] = result;
+    }
+    
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        int[] alchol = new int[n];
+        for (int i = 0; i < n; i++)
+            alchol[i] = Integer.parseInt(in.readLine());
+        System.out.println(solve(n, alchol));
+    }
+}
--- DP/9095/Main.java
+++ DP/9095/Main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 public class Main
 {
     private static int[] cache;
@@ -26,13 +31,14 @@ public class Main
         return cache[n] = oneTwoThree(n-3) + oneTwoThree(n-2) + oneTwoThree(n-1);
     }
     
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int test = Integer.parseInt(in.readLine());
         for (int i = 0; i < test; i++)
         {
-            int n = sc.nextInt();
+            int n = Integer.parseInt(in.readLine());
             System.out.println(solve(n));
         }
     }
