b86c2dfb096762d0630baf869a33d4c71c7b0c2f
HwangJinSeong <progresivoJS@gmail.com>
2017-08-29 00:10:53 +0900

    9465

--- DP/9465/Main.java
+++ DP/9465/Main.java
@@ -2,9 +2,62 @@ import java.util.*;
 import java.io.*;
 public class Main
 {
+    private static int[][] cache;
+    private static int[][] stickers;
+    private static int n;
+    public static int solve(int[][] stickers)
+    {
+        Main.stickers = stickers;
+        n = stickers[0].length;
+        cache = new int[2][n];
+        for (int i = 0; i < cache.length; i++)
+            Arrays.fill(cache[i], -1);
+
+        return Math.max(cut(0, 0), cut(1, 0));
+    }
+
+    private static int cut(int row, int col)
+    {
+        if (row >= 2)
+            return 0;
+        if (col == n-1)
+            return stickers[row][col];
+        if (col > n-1)
+            return 0;
+
+        if (cache[row][col] != -1)
+            return cache[row][col];
+
+        int result = 0;
+        if (row == 0)
+        {
+            result = Math.max(result, stickers[row][col] + cut(1, col + 1));
+            result = Math.max(result, stickers[row][col] + cut(1, col + 2));
+            result = Math.max(result, stickers[row][col] + cut(0, col + 2));
+        }
+        else
+        {
+            result = Math.max(result, stickers[row][col] + cut(0, col + 1));
+            result = Math.max(result, stickers[row][col] + cut(0, col + 2));
+            result = Math.max(result, stickers[row][col] + cut(1, col + 2));
+        }
+
+        return cache[row][col] = result;
+    }
+
     public static void main(String[] args)
     {
         In.init();
+        int test = In.nextInt();
+        for (int i = 0; i < test; i++)
+        {
+            int n = In.nextInt();
+            int[][] stickers = new int[2][n];
+            for (int row = 0; row < 2; row++)
+                for (int col = 0; col < n; col++)
+                    stickers[row][col] = In.nextInt();
+            System.out.println(solve(stickers));
+        }
     }
 
     private static class In
@@ -14,15 +67,7 @@ public class Main
 
         public static void init()
         {
-            br = new BufferedReader(new InputStreamReader(System.in));
-            // try
-            // {
-            //     br = new BufferedReader(new FileReader("/home/ubuntu/workspace/data.txt"));
-            // }
-            // catch(Exception e)
-            // {
-            //     e.printStackTrace();
-            // }
+             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
         public static String next()
@@ -47,3 +92,4 @@ public class Main
         }
     }
 }
+
