1fca32b0e46dbdb4a7d5cc3a1e4c647c083fc938
progresivoJS <progresivoJS@gmail.com>
2017-09-09 07:39:01 +0000

    io problems

---
+++ IO/5339/Main.java
@@ -0,0 +1,22 @@
+/**
+ * problem 5339
+ * 콜센터
+ * https://www.acmicpc.net/problem/5339
+ * written by progresivoJS
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\ ");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
