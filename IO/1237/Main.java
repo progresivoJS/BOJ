33bc711cee64de0b50e570ec6aeeacb4cb11a94e
progresivoJS <progresivoJS@gmail.com>
2017-09-09 07:23:02 +0000

    1237

---
+++ IO/1237/Main.java
@@ -0,0 +1,13 @@
+/**
+ * problem 1237
+ * 정ㅋ벅ㅋ
+ * https://www.acmicpc.net/problem/1237
+ * written by progresivoJS
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("문제의 정답");
+    }
+}
