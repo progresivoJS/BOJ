c83d5f69e47a21ad2efd60b3562d8727fcd621fe
progresivoJS <progresivoJS@gmail.com>
2017-09-09 07:19:00 +0000

    11719

---
+++ IO/11719/Main.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * problem 11719
+ * 그대로 출력하기 2
+ * https://www.acmicpc.net/problem/11719
+ * written by progresivoJS
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        In.init();
+        String buffer;
+        while ((buffer = In.nextLine()) != null)
+            System.out.println(buffer);
+    }
+    
+    private static class In
+    {
+        private static BufferedReader br;
+        private static StringTokenizer st;
+    
+        public static void init()
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            try
+            {
+                br = new BufferedReader(new FileReader("/home/ubuntu/workspace/data.txt"));
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        
+        public static String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    
+        public static String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public static int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+    }
+}
