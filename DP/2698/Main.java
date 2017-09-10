948fce2395cfdf9f40d750c95dd2016378644149
progresivoJS <progresivoJS@gmail.com>
2017-09-10 14:26:54 +0000

    2698

--- DP/2698/Main.java
+++ DP/2698/Main.java
@@ -1,31 +1,94 @@
-import java.util.Scanner;
-import java.io.File;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
+
+/**
+ * problem 2698
+ * 인접한 비트의 개수
+ * https://www.acmicpc.net/problem/2698
+ * written by progresivoJS
+ */
 public class Main
 {
-    public static int solve(int n, int k)
+    private static int[][] cache;
+    private static void solve(int test)
     {
+        cache = new int[101][101];
+        for (int i = 0; i < cache.length; i++)
+            Arrays.fill(cache[i], -1);
         
+        for (int i = 0; i < test; i++)
+        {
+            int n = In.nextInt();
+            int k = In.nextInt();
+            
+            System.out.println(bitCount(n, k));
+        }
+    }
+    
+    private static int bitCount(int n, int k)
+    {
+        if (n == 1 && k == 0)
+            return 2;
+        else if (n == 2 && k == 0)
+            return 3;
+        else if (n-2 == k)
+            return 2;
+        else if (n-1 == k)
+            return 1;
+            
+        if (cache[n][k] != -1)
+            return cache[n][k];
+            
+        int result = 0;
+        result += bitCount(n-1, k);
+        
+        int i = k;
+        int j = n-2;
+        while (i >= 0)
+        {
+            result += bitCount(j, i);
+            i--;
+            j--;
+        }
+        return cache[n][k] = result;
     }
     
     public static void main(String[] args)
     {
-        Scanner sc = null;
-        try
+        In.init();
+        int test = In.nextInt();
+        solve(test);
+    }
+    
+    private static class In
+    {
+        private static BufferedReader br;
+        private static StringTokenizer st;
+    
+        public static void init()
         {
-            sc = new Scanner(new File("/home/ubuntu/workspace/data.txt"));
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
-        catch (Exception e)
+    
+        public static String next()
         {
-            e.printStackTrace();
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
         }
-        
-        int test = sc.nextInt();
-        for (int i = 0; i < test; i++)
+    
+        public static int nextInt()
         {
-            int n = sc.nextInt();
-            int k = sc.nextInt();
-            System.out.println(solve(n, k));
+            return Integer.parseInt(next());
         }
     }
 }
