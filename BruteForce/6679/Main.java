d7f0529bc69d669b5cac0f5e1999e1c45f7b82f4
progresivoJS <progresivoJS@gmail.com>
2017-09-15 14:38:30 +0000

    6679

--- BruteForce/6679/Main.java
+++ BruteForce/6679/Main.java
@@ -14,84 +14,27 @@ public class Main
         In.init();
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 1000; i <= 9999; i++)
-            if (isInteresting(i))
+        {
+            int sum = sum(i, 10);
+            if (sum == sum(i, 12) && sum == sum(i, 16))
             {
                 out.write(String.valueOf(i));
                 out.write("\n");
             }
+        }
             
         out.close();
     }
     
-    public static boolean isInteresting(int number)
+    public static int sum(int number, int radix)
     {
-        int ten = 0;
-        String str10 = String.valueOf(number);
-        for (int i = 0; i < str10.length(); i++)
-            ten += str10.charAt(i) - '0';
-        
-        int twelve = 0;
-        String str12 = Integer.toString(number, 12);
-        for (int i = 0; i < str12.length(); i++)
+        int result = 0;
+        while (number > 0)
         {
-            if (str12.charAt(i) >= 'a')
-                twelve += str12.charAt(i) - 'a' + 10;
-            else
-                twelve += str12.charAt(i) - '0';
+            result += number % radix;
+            number /= radix;
         }
         
-        int sixteen = 0;
-        String str16 = Integer.toString(number, 16);
-        for (int i = 0; i < str16.length(); i++)
-        {
-            if (str16.charAt(i) >= 'a')
-                sixteen += str16.charAt(i) - 'a' + 10;
-            else
-                sixteen += str16.charAt(i) - '0';
-        }
-        
-        if (ten == twelve && twelve == sixteen)
-            return true;
-        return false;
-    }
-    
-    private static class In
-    {
-        private static BufferedReader br;
-        private static StringTokenizer st;
-    
-        public static void init()
-        {
-            br = new BufferedReader(new InputStreamReader(System.in));
-            try
-            {
-                br = new BufferedReader(new FileReader("/home/ubuntu/workspace/data.txt"));
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-    
-        public static String next()
-        {
-            while (st == null || !st.hasMoreElements())
-            {
-                try
-                {
-                    st = new StringTokenizer(br.readLine());
-                }
-                catch (IOException  e)
-                {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-    
-        public static int nextInt()
-        {
-            return Integer.parseInt(next());
-        }
+        return result;
     }
 }
