7686cdedc8c968646aff6dd8d64a70cc2aff0af3
progresivoJS <progresivoJS@gmail.com>
2017-09-16 14:10:02 +0000

    10815

--- Bisection/2805/Main.java
+++ Bisection/2805/Main.java
@@ -9,10 +9,42 @@ import java.io.*;
  */
 public class Main
 {
+    // 나무 높이 seq 배열이 주어질 때, m 길이 이상 만큼을 자르기 위한 높이의 최댓값.
+    private static int maxHeight(int[] seq, int m)
+    {
+        int lo = 0;
+        int hi = 1000000000;
+        
+        while (lo + 1 < hi)
+        {
+            int mid = (lo + hi) / 2;
+            long cut = 0;
+            for (int i = 0; i < seq.length; i++)
+            {
+                if (seq[i] > mid)
+                    cut += seq[i] - mid;
+                if (cut >= m)
+                    break;
+            }
+            
+            if (cut < m)
+                hi = mid;
+            else
+                lo = mid;
+        }
+        return lo;
+    }
+    
     public static void main(String[] args)
     {
         In.init();
+        int n = In.nextInt();
+        int m = In.nextInt();
+        int[] seq = new int[n];
+        for (int i = 0; i < n; i++)
+            seq[i] = In.nextInt();
         
+        System.out.println(maxHeight(seq, m));
     }
     
     private static class In
