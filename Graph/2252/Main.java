04fb2cf818492dfe11f7433de385528bf0a80292
progresivoJS <progresivoJS@gmail.com>
2017-09-28 11:10:22 +0000

    2252

---
+++ Graph/2252/Main.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * problem 2252
+ * 줄 세우기
+ * https://www.acmicpc.net/problem/2252
+ * written by progresivoJS on 2017.09.28
+ */
+public class Main
+{
+    private static Deque<Integer> reversePostOrder;
+    private static boolean[] marked;
+    public static void solve(LinkedList<Integer>[] adj)
+    {
+        reversePostOrder = new ArrayDeque<Integer>();
+        marked = new boolean[adj.length];
+        
+        for (int v = 0; v < adj.length; v++)
+            if (!marked[v])
+                dfs(adj, v);
+                
+        StringBuilder str = new StringBuilder();
+        for (Integer i : reversePostOrder)
+            str.append(i + 1).append(' ');
+        System.out.println(str.toString());
+    }
+    
+    private static void dfs(LinkedList<Integer>[] adj, int v)
+    {
+        marked[v] = true;
+        for (int w : adj[v])
+            if (!marked[w])
+                dfs(adj, w);
+        reversePostOrder.push(v);
+    }
+    
+    public static void main(String[] args)
+    {
+        In.init();
+        int n = In.nextInt();
+        int m = In.nextInt();
+        
+        LinkedList<Integer>[] adj = (LinkedList<Integer>[]) new LinkedList[n];
+        for (int i = 0; i < n; i++)
+            adj[i] = new LinkedList<>();
+            
+        for (int i = 0; i < m; i++)
+            adj[In.nextInt() - 1].add(In.nextInt() - 1);
+        solve(adj);
+    }
+    
+    private static class In
+    {
+        private static BufferedReader br;
+        private static StringTokenizer st;
+    
+        public static void init()
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            try
+            {
+                br = new BufferedReader(new FileReader("/home/ubuntu/workspace/data.txt"));
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    
+        public static String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public static int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+    }
+}
